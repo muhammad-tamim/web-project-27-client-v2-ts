404eb90c3dbfc11afa13ec14dae1e8130a462a80
muhammad-tamim <contact2tamim@gmail.com>
2026-01-15 02:56:16 +0600

    feat(utils): add navItems

---
+++ src/utils/navItems.ts
@@ -0,0 +1,6 @@
+export const navItems = [
+    { id: 1, to: "/", label: "Home" },
+    { id: 2, to: "/all-recipes", label: "All Recipes" },
+    { id: 3, to: "/my-recipes", label: "My Recipes" },
+    { id: 4, to: "/add-recipes", label: "Add Recipes" },
+]
