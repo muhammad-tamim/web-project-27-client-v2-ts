dd01d9c4cc15b17769b6de39d6aaf75ac0e2541f
muhammad-tamim <contact2tamim@gmail.com>
2026-01-17 17:27:36 +0600

    (component / structure / home): fix category id typing mistake

--- src/components/structure/Home/Categories.tsx
+++ src/components/structure/Home/Categories.tsx
@@ -12,23 +12,23 @@ const Categories = () => {
             <div className='max-w-7xl mx-auto'>
                 <h2 className='text-3xl md:text-5xl text-center font-semibold mb-20'>Recipe Categories</h2>
                 <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-10 justify-items-center'>
-                    <Link to={'/recipe-category/:id'}>
+                    <Link to={'/recipe-category/:appetizers'}>
                         <img src={category1} alt="image" className='rounded-full w-48 hover:rotate-12 transition-transform duration-500' />
                         <p className='text-center text-lg font-medium mt-2'>Appetizers</p>
                     </Link>
-                    <Link to={'/recipe-category/:id'}>
+                    <Link to={'/recipe-category/:beef'}>
                         <img src={category2} alt="image" className='rounded-full w-48 hover:rotate-12 transition-transform duration-500' />
                         <p className='text-center text-lg font-medium mt-5'>Appetizers</p>
                     </Link>
-                    <Link to={'/recipe-category/:id'}>
+                    <Link to={'/recipe-category/:baked'}>
                         <img src={category3} alt="image" className='rounded-full w-48 hover:rotate-12 transition-transform duration-500' />
                         <p className='text-center text-lg font-medium mt-5'>Appetizers</p>
                     </Link>
-                    <Link to={'/recipe-category/:id'}>
+                    <Link to={'/recipe-category/:vegetables'}>
                         <img src={category4} alt="image" className='rounded-full w-48 hover:rotate-12 transition-transform duration-500' />
                         <p className='text-center text-lg font-medium mt-5'>Appetizers</p>
                     </Link>
-                    <Link to={'/recipe-category/:id'}>
+                    <Link to={'/recipe-category/:desserts'}>
                         <img src={category5} alt="image" className='rounded-full w-48 hover:rotate-12 transition-transform duration-500' />
                         <p className='text-center text-lg font-medium mt-5'>Appetizers</p>
                     </Link>
