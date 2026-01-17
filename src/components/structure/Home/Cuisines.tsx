1e74f56ebf4bb1be6dd656b21af59ac4f4d970b3
muhammad-tamim <contact2tamim@gmail.com>
2026-01-17 19:59:00 +0600

    feat(Home Page): add cuisine component

---
+++ src/components/structure/Home/Cuisines.tsx
@@ -0,0 +1,46 @@
+import { Link } from "react-router";
+
+import indian from '../../../assets/images/indian-flag.avif'
+import mexican from '../../../assets/images/Mexican-flag.png'
+import italian from '../../../assets/images/italian-flag.avif'
+import chinese from '../../../assets/images/chaines-flag.webp'
+import korean from '../../../assets/images/korian-flag.webp'
+import japanese from '../../../assets/images/japanese-flag.jpg'
+
+const Cuisines = () => {
+    return (
+        <div className='bg-gray-50 py-10'>
+            <div className='max-w-7xl mx-auto'>
+                <h2 className='text-3xl md:text-5xl text-center font-semibold mb-20'>Recipe Cuisine</h2>
+                <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-10 justify-items-center'>
+                    <Link to={'/recipe-cuisine/:india'}>
+                        <img src={indian} alt="image" className='rounded-full object-cover size-48 hover:rotate-12 transition-transform duration-500' />
+                        <p className='text-center text-lg font-medium mt-2'>Indian</p>
+                    </Link>
+                    <Link to={'/recipe-cuisine/:mexico'}>
+                        <img src={mexican} alt="image" className='rounded-full object-cover size-48 hover:rotate-12 transition-transform duration-500' />
+                        <p className='text-center text-lg font-medium mt-5'>Mexican</p>
+                    </Link>
+                    <Link to={'/recipe-cuisine/:itali'}>
+                        <img src={italian} alt="image" className='rounded-full object-cover size-48 hover:rotate-12 transition-transform duration-500' />
+                        <p className='text-center text-lg font-medium mt-5'>Italian</p>
+                    </Link>
+                    <Link to={'/recipe-cuisine/:china'}>
+                        <img src={chinese} alt="image" className='rounded-full object-cover size-48 hover:rotate-12 transition-transform duration-500' />
+                        <p className='text-center text-lg font-medium mt-5'>Chinese</p>
+                    </Link>
+                    <Link to={'/recipe-cuisine/:korea'}>
+                        <img src={korean} alt="image" className='rounded-full object-cover size-48 hover:rotate-12 transition-transform duration-500' />
+                        <p className='text-center text-lg font-medium mt-5'>Korean</p>
+                    </Link>
+                    <Link to={'/recipe-cuisine/:japan'}>
+                        <img src={japanese} alt="image" className='rounded-full object-cover size-48 hover:rotate-12 transition-transform duration-500' />
+                        <p className='text-center text-lg font-medium mt-5'>Japanese</p>
+                    </Link>
+                </div >
+            </div >
+        </div>
+    );
+};
+
+export default Cuisines;
