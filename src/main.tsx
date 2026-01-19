b82d187f16d7a81ee90fd7c814f95c9bb57e3cb1
muhammad-tamim <contact2tamim@gmail.com>
2026-01-20 01:52:22 +0600

    feat: add authProvider

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import './index.css'
 import { RouterProvider } from 'react-router';
 import { routes } from './routes/Routes';
 import MaxWidth from './components/ui/MaxWidth';
+import AuthProvider from './providers/AuthProvider';
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <MaxWidth>
-      <RouterProvider router={routes}></RouterProvider>
+      <AuthProvider>
+        <RouterProvider router={routes}></RouterProvider>
+      </AuthProvider>
     </MaxWidth>
   </StrictMode>,
 )
