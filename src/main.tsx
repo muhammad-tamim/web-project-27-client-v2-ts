8abbaeab2a9fd414be9a4694c21cb3ad23beb832
muhammad-tamim <contact2tamim@gmail.com>
2026-01-20 03:02:50 +0600

    feat: add react-hot-toast

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,14 @@ import { RouterProvider } from 'react-router';
 import { routes } from './routes/Routes';
 import MaxWidth from './components/ui/MaxWidth';
 import AuthProvider from './providers/AuthProvider';
+import { Toaster } from 'react-hot-toast';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <MaxWidth>
       <AuthProvider>
         <RouterProvider router={routes}></RouterProvider>
+        <Toaster position='top-right'></Toaster>
       </AuthProvider>
     </MaxWidth>
   </StrictMode>,
