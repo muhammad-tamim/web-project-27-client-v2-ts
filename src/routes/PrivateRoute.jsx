807ce0643b7c9fc9187998bf3c6e059b377731b2
muhammad-tamim <contact2tamim@gmail.com>
2026-01-20 02:08:37 +0600

    feat: make profile, add and my recipe page to private

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,6 +1,6 @@
 import React, { use } from 'react';
-import { AuthContext } from '../context/AuthContext';
 import { Navigate, useLocation } from 'react-router';
+import { AuthContext } from '../contexts/AuthContext';
 
 const PrivateRoute = ({ children }) => {
     const location = useLocation();
