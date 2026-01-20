bd99ddb7903f8cfef5c5cfba440f52e37edd9d1b
muhammad-tamim <contact2tamim@gmail.com>
2026-01-20 22:51:10 +0600

    feat(AllRecipePage): implement backend logic

--- src/pages/AllRecipesPage.tsx
+++ src/pages/AllRecipesPage.tsx
@@ -1,22 +1,30 @@
-import image1 from '../assets/images/baked-chicken-breast.jpg'
-import image2 from '../assets/images/chicken-tikka-masala.jpg'
-import image3 from '../assets/images/burger.jpg'
-import image4 from '../assets/images/raspberry.jpg'
 import bg from '../assets/images/Recipes-background1.jpg'
 
 import PagesBanner from '../components/ui/PagesBanner';
 import Cards from '../components/ui/Cards';
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import LoadingSpinner from '../components/ui/LoadingSpinner';
 
-const slideItems = [
-    { id: 1, title: 'Baked Chicken Breast', image: image1, rating: 4, description: 'A handful of simple ingredients typify the fresh, vibrant flavors of Greek cooking Lorem ipsum dolor sit amet..', author: 'Md. Tamim', authorImage: "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" },
-    { id: 2, title: 'Chicken Tikka Masala', image: image2, rating: 5, description: 'A handful of simple ingredients typify the fresh, vibrant Lorem ipsum dolor sit amet. flavors of Greek cooking.', author: 'Md. Tamim', authorImage: "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" },
-    { id: 3, title: 'Ultimate Cheese Burger', image: image3, rating: 4, description: 'A handful of simple ingredients Lorem ipsum dolor sit amet. typify the fresh, vibrant flavors of Greek cooking.', author: 'Md. Tamim', authorImage: "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" },
-    { id: 4, title: 'Raspberry Tartlet', image: image4, rating: 3, description: 'A handful Lorem ipsum dolor sit amet. of simple ingredients typify the fresh, vibrant flavors of Greek cooking.', author: 'Md. Tamim', authorImage: "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" },
-    { id: 5, title: 'Ultimate Cheese Burger', image: image3, rating: 4, description: 'A handful of simple ingredients Lorem ipsum dolor sit amet. typify the fresh, vibrant flavors of Greek cooking.', author: 'Md. Tamim', authorImage: "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" },
-    { id: 6, title: 'Raspberry Tartlet', image: image4, rating: 3, description: 'A handful Lorem ipsum dolor sit amet. of simple ingredients typify the fresh, vibrant flavors of Greek cooking.', author: 'Md. Tamim', authorImage: "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" },
-]
 
 const AllRecipesPage = () => {
+
+    const [recipes, setRecipes] = useState([])
+    const [pending, setPending] = useState(true);
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/recipes')
+            .then(res => {
+                setRecipes(res.data)
+                setPending(false)
+            })
+    }, [])
+
+    if (pending) {
+        return <LoadingSpinner minHScreen={'min-h-screen'}></LoadingSpinner>
+    }
+
+
     return (
         <div>
             <PagesBanner title={"All Recipes"} bg={bg}></PagesBanner>
@@ -24,8 +32,8 @@ const AllRecipesPage = () => {
                 <div className='max-w-7xl mx-auto'>
                     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center gap-10 px-5 xl:px-0'>
 
-                        {slideItems.map((item) =>
-                            <Cards item={item}></Cards>
+                        {recipes.map((recipe) =>
+                            <Cards key={recipe._id} recipe={recipe}></Cards>
                         )}
 
                     </div >
